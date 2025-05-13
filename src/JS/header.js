7d02d209edd44c149a2c1a9c849c82d8ae894209
alaaAdel <alaaward3000@gmail.com>
2025-05-13 11:11:27 +0300

    add product section

--- src/JS/header.js
+++ src/JS/header.js
@@ -76,3 +76,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+
+let links = document.querySelectorAll(".products .filter li");
+
+
+links.forEach((p)=>{
+    p.onclick = () => {
+      document.querySelector(".products .filter li.active").classList.remove("active");
+      p.classList.add("active");
+      let tab = p.getAttribute("data-active");
+      document.querySelector(".cards.active-tab").classList.remove("active-tab");
+      document.querySelector(tab).classList.add("active-tab");
+    
+    }
+  
+  })
